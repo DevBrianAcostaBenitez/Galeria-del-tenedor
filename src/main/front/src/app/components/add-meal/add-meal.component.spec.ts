11b15fb4ed7699fd78d423bb211d6304e35db3f1
DevBrianAcostaBenitez <brianacostabenitez@gmail.com>
2023-11-27 16:50:13 +0000

    .

--- src/main/front/src/app/components/add-meal/add-meal.component.spec.ts
+++ src/main/front/src/app/components/add-meal/add-meal.component.spec.ts
@@ -1,19 +1,22 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router'; 
 import { AddMealComponent } from './add-meal.component';
-
 describe('AddMealComponent', () => {
   let component: AddMealComponent;
   let fixture: ComponentFixture<AddMealComponent>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [AddMealComponent]
+      declarations: [AddMealComponent],
+      imports: [HttpClientTestingModule,FormsModule, RouterModule],
     });
     fixture = TestBed.createComponent(AddMealComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
+
 
   it('should create', () => {
     expect(component).toBeTruthy();
